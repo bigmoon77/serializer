02931bad9f6a37f03c33e0edf0535e996490bc15
bigmoon77 <briefs1919810@gmail.com>
2026-04-03 04:28:39 +0900

    ファイル整理 mono_resource_mapをファイル分けしました

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include "serialize/hold_object.h"
 #include "serialize/error/located_exception.h"
+#include "serialize/mono_resource_map.h"
 
 using namespace std;
 template<typename t>
@@ -38,125 +39,6 @@ struct shader_literate {
 
 
 using namespace serializer;
-
-/// <summary>
-/// pathの参照数を見る
-/// リソースをメモリに複数展開しない為
-/// </summary>
-struct mono_resource_map {
-
-public:
-
-	struct iliterate {
-		virtual void read(void*, const std::filesystem::path&) = 0;
-		virtual void write(void*, const std::filesystem::path&) = 0;
-	};
-
-	template<typename t>
-	struct literate;
-
-	//定義例とデフォルトルート
-	//このmapで扱うリソースに固有読み書きを定義する場合
-	//literateは iliterateを継承する必要がある
-
-	template<typename t>
-	struct literate : public iliterate {
-
-		void read(void* obj, const std::filesystem::path& path)override {
-			default_literate<t> literate;
-			literate.read(*(t*)obj, path);
-		}
-		void write(void* obj, const std::filesystem::path& path) override {
-			default_literate<t> literate;
-			literate.write(*(t*)obj, path);
-		}
-	};
-
-
-private:
-	struct resource_info {
-		size_t usecount = 0;
-		std::type_index tid = typeid(void);
-		std::unique_ptr<iliterate> liter;
-		std::shared_ptr<void> obj;//型を抽象化する為にshared ptr
-
-		resource_info() = default;
-
-
-		void addref() {
-			++usecount;
-		}
-		void release() {
-			--usecount;
-		}
-
-		bool nouse()const {
-			return usecount == 0;
-		}
-	};	
-	std::map<std::filesystem::path, resource_info> _map;
-	std::mutex _mtx;
-
-public:
-
-	template<typename value_type>
-	void set(const std::filesystem::path& path) {
-		std::lock_guard lock(_mtx);
-		auto& target = _map[path];
-		target.tid = typeid(value_type);
-		target.liter.reset(new literate<value_type>());
-	}
-
-	template<typename value_type>
-	auto use(const std::filesystem::path& path) {
-		std::lock_guard lock(_mtx);
-		auto& target = _map[path];
-
-#ifdef _DEBUG
-		if (target.tid != typeid(value_type)) {
-			throw error::located_exception("型の不一致");
-		}
-#endif
-
-		if (!target.obj) {//未定義
-
-			auto temp = std::make_shared<value_type>();
-			target.liter->read(temp.get(), path);
-			target.obj = temp;
-		}
-
-		target.addref();
-		
-		return std::static_pointer_cast<value_type>(target.obj);
-	}
-
-	template<typename value_type>
-	void unuse(const std::filesystem::path& path) {
-		std::lock_guard lock(_mtx);
-		auto& target = _map[path];
-
-#ifdef _DEBUG
-		if (target.tid != typeid(value_type)) {
-			throw error::located_exception("型の不一致");
-		}
-		if (target.nouse()) {
-			//release前に0なのはおかしい
-			//健全なプログラマーの為にthrow
-			throw serializer::error::located_exception("使用回数、解放回数の不一致");
-		}
-#endif 
-
-		target.release();
-
-		if (target.nouse()) {//参照カウント0
-
-			target.liter->write(target.obj.get(), path);
-			target.obj.reset();
-		}
-	}
-
-};
-
 
 int main() {
 	using namespace std;
