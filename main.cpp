61737368a02c423d5b2a586357e85cab4a19c1a1
bigmoon77 <162124307+bigmoon77@users.noreply.github.com>
2026-06-12 12:41:40 +0900

    stlイテレータ対応の前座 帰ったら続きを作る

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <filesystem>
 #include <iostream>
 #include <map>
+#include <vector>
+#include <list>
+
 #include <mutex>
 #include <typeindex>
 
@@ -38,14 +41,165 @@ struct shader_literate {
 };
 
 
+namespace serializer {
+
+	template<typename _t>
+	struct serialize_traits<std::vector<_t>> {
+		using t = std::vector<_t>;
+		
+		/// <summary>
+		/// オブジェクトをビット列に変換する
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
+		inline std::vector<std::uint8_t> operator()(const t& a) const {
+			std::vector <std::uint8_t> res(sizeof(size_t) + sizeof(typename t::value_type) * a.size());
+			size_t size = a.size();
+
+			std::memcpy(res.data(),& size , sizeof(size_t));//サイズ入力
+
+			//size_t offset = sizeof(size_t);
+			serialize_traits<typename t::value_type> ele_t;
+
+			for (auto& i : a)
+			{
+				auto temp = ele_t(i);
+				res.insert(res.end(), temp.begin(), temp.end());//必ず同じサイズかわからん
+				//offset += temp.size();
+			}
+
+			return res;
+		}
+
+		/// <summary>
+		/// ビット列からオブジェクトを初期化する
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="bin"></param>
+		inline void store(t& a, const std::vector<std::uint8_t>& bin)const {
+			
+			{
+				size_t size;
+				std::memcpy(&size, bin.data(), sizeof(size_t));
+				a.resize(size);
+			}
+
+			size_t offset = sizeof(size_t);
+			serialize_traits<typename t::value_type> ele_t;
+			
+			auto head = bin.data();
+			for (auto& i : a)
+			{
+				offset += ele_t.store(i, head + offset);
+			}
+		}
+
+		/// <summary>
+		/// ビット列からオブジェクトを初期化する
+		/// return of loaded bytes
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="bin"></param>
+		/// <returns></returns>
+		inline size_t store(t& a, const std::uint8_t* bin)const {
+
+			{
+				size_t size;
+				std::memcpy(&size, bin, sizeof(size_t));
+				a.resize(size);
+			}
+
+			size_t offset = sizeof(size_t);
+			serialize_traits<typename t::value_type> ele_t;
+
+			auto head = bin;
+			for (auto& i : a)
+			{
+				offset += ele_t.store(i, head + offset);
+			}
+			
+			return offset;
+		}
+	};
+
+
+	template<typename _t,typename _u>
+	struct serialize_traits<std::pair<_t,_u>> {
+		using t = std::pair<_t,_u>;
+
+		/// <summary>
+		/// オブジェクトをビット列に変換する
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
+		inline std::vector<std::uint8_t> operator()(const t& a) const {
+			std::vector <std::uint8_t> res;
+			
+			serialize_traits<_t> tt;
+			serialize_traits<_u> ut;
+
+			auto temp = tt(a.first);
+			res.insert(res.end(), temp.begin(), temp.end());
+
+			temp = ut(a.second);
+			res.insert(res.end(), temp.begin(), temp.end());
+
+
+			return res;
+		}
+
+		/// <summary>
+		/// ビット列からオブジェクトを初期化する
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="bin"></param>
+		inline void store(t& a, const std::vector<std::uint8_t>& bin)const {
+
+			serialize_traits<_t> tt;
+			serialize_traits<_u> ut;
+			
+			auto offset = tt.store(a.first, bin.data());
+			ut.store(a.second, bin.data() + offset);
+
+		}
+
+		/// <summary>
+		/// ビット列からオブジェクトを初期化する
+		/// return of loaded bytes
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="bin"></param>
+		/// <returns></returns>
+		inline size_t store(t& a, const std::uint8_t* bin)const {
+
+			serialize_traits<_t> tt;
+			serialize_traits<_u> ut;
+
+			auto offset = tt.store(a.first, bin);
+			offset += ut.store(a.second, bin + offset);
+
+			return offset;
+		}
+	};
+
+}
+
+
+
 using namespace serializer;
+
 
 int main() {
 	using namespace std;
 	using namespace serializer;
 
+	std::vector<std::pair<int, int>> a;
+
+	hold_object<std::vector<std::pair<int, int>>> lockobj(a);
 
 
+
+	return 0;
 	mono_resource_map map;
 	map.set<int>("hoge");
 	
